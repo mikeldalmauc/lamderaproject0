27ccab19b6e268c5a7289579ebec9d6efe874ccd
mikeldalmauc <mikeldalmauc@gmail.com>
2023-11-09 10:11:56 +0100

    Preparing for v2

--- src/Evergreen/V2/Types.elm
+++ src/Evergreen/V2/Types.elm
@@ -6,16 +6,8 @@ import Evergreen.V2.Crate
 import Url
 
 
-type alias Question =
-    { question : String
-    , options : List String
-    }
-
-
 type alias FrontendModel =
     { key : Browser.Navigation.Key
-    , message : String
-    , question : Question
     , crate : Evergreen.V2.Crate.Model
     }
 
